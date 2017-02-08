b5598bc8fe86206769b3dccaae6e9fa1788dc1c2
Amir Ben Ari <benari.amir@gmail.com>
2017-02-08 14:43:47 +0200

    added stupid test

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+describe('test', function() {
+    it('Just a small true test', function() {
+        expect(true).to.be.true;
+    });
+});
