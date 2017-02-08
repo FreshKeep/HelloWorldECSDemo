fc864d32a8f66a6af0c12e1c5513d29af673e000
Amir Ben Ari <benari.amir@gmail.com>
2017-02-08 16:26:52 +0200

    added full test

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,37 @@
 'use strict'
+var path = require('path');
+var winstonConf = require('winston-config');
+var winston = require('winston');
+global.conf = require('../config/appsettings');
+
+winstonConf.fromFileSync(path.join(__dirname, '../config/winston-config.json'), function(error) {
+    if (error) {
+        console.log('error during winston configuration');
+    } else {
+        console.log('everything alright');
+    }
+});
+
+var testLogger = winston.loggers.get('application');
 
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
+var app = require('../app.js');
+var request = require('request-promise');
 
-describe('test', function() {
-    it('Just a small true test', function() {
-        expect(true).to.be.true;
+describe('Hello World Test', function() {
+    it('Run app perform GET, match text', function*() {
+        var reply = yield request('http://localhost:3000');
+        testLogger.verbose(reply);
+        expect(reply).to.eql(global.conf.message);
+    });
+
+    it('Run app perform GET, full reply', function*() {
+        var reply = yield request({
+            uri: 'http://localhost:3000',
+            resolveWithFullResponse: true
+        });
+        expect(reply.statusCode).to.eql(200);
+        expect(reply.body).to.eql(global.conf.message);
     });
 });
